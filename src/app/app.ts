bed4e85492ca60e66722626858641ca715563aba
Ilya Katlinsky <ilya.katlinsky@gmail.com>
2015-12-21 10:05:46 +0300

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'markdown',
+    template: '<div>My app is working!!!</div>'
+})
+
+export class MarkdownAppComponent {
+    constructor() {}
+}
---
+++ src/app/boot.ts
@@ -0,0 +1,5 @@
+import 'angular2/bundles/angular2-polyfills';
+import {bootstrap} from 'angular2/platform/browser';
+import {MarkdownAppComponent} from './app';
+
+bootstrap(MarkdownAppComponent);
